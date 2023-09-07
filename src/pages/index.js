696c9bb5b3185333e11e1a3903e381f0cee69fe9
Sanjeev Ranjan Singh <100428475+SanjeevSingh08@users.noreply.github.com>
2023-09-07 13:20:03 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,22 +53,48 @@ export default function Home() {
     })
   }
 
-  const isSmallScreen = window.innerWidth<500; // Check if the screen width is less than 500px
-
   return (
     <div className="main-container container mx-auto max-w-[700px]">
       <div className="flex flex-col h-screen bg-gray-900">
-        <div className={`p-6 ${isSmallScreen ? 'fixed top-0 left-0 right-0 bg-white text-black' : ''}`}>
-          <h1 className={`bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-left font-bold text-${isSmallScreen ? '2xl' : '4xl'} md:text-6xl`}>56765 MTN</h1>
+        <div className="p-6">
+          <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-left font-bold text-4xl md:text-6xl">56765 MTN</h1>
           <h4 className="text-white text-center py-3 text-sm md:text-base">Hi, Welcome to the SMS CHATGPT Service, you can ask questions, and AI will help you</h4>
         </div>
-        <div className={`flex-grow p-6 chat-section ${isSmallScreen ? 'mt-20' : ''}`} style={{ overflowY: 'auto' }} ref={chatContainerRef}>
+        <div className="flex-grow p-6 chat-section" style={{ overflowY: 'auto' }} ref={chatContainerRef}>
           <div className="flex flex-col space-y-4">
-            {/* ... your chat content */}
+            {chatLog.map((message, index) => (
+              <div key={index} className={`flex ${message.type === 'user' ? 'justify-end' : 'justify-start'}`}>
+                <div className={`${message.type === 'user' ? 'bg-purple-500' : 'bg-gray-800'} rounded-lg p-2 text-white max-w-sm text-sm md:text-base`}>
+                  {message.message}
+                </div>
+              </div>
+            ))}
+            {isLoading && (
+              <div key={chatLog.length} className="flex justify-start">
+                <div className="bg-gray-800 rounded-lg p-2 text-white max-w-sm text-sm md:text-base">
+                  <TypingAnimation />
+                </div>
+              </div>
+            )}
           </div>
         </div>
-        <form onSubmit={handleSubmit} className={`flex-none p-6 type-section ${isSmallScreen ? 'fixed bottom-0 left-0 right-0' : ''}`}>
-          {/* ... your form content */}
+        <form onSubmit={handleSubmit} className="flex-none p-6 type-section"
+  style={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+          <div className="flex rounded-lg border border-gray-700 bg-gray-800">
+            <input
+              type="text"
+              className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none text-sm md:text-base"
+              placeholder="Type your message..."
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300"
+            >
+              Send
+            </button>
+          </div>
         </form>
       </div>
     </div>
